17069d6ab70eb0e04c4ff14e1c4ed2f38fd6fc27
qiukai <qiukai1@360.cn>
2020-11-18 17:58:45 +0800

    拒绝抢占该m和释放m

--- src/runtime/runtime1.go
+++ src/runtime/runtime1.go
@@ -439,6 +439,7 @@ func timediv(v int64, div int32, rem *int32) int32 {
 // Helpers for Go. Must be NOSPLIT, must only call NOSPLIT functions, and must not block.
 
 //go:nosplit
+// 当前m禁止抢占
 func acquirem() *m {
 	_g_ := getg()
 	_g_.m.locks++
@@ -446,6 +447,7 @@ func acquirem() *m {
 }
 
 //go:nosplit
+// 释放m抢占
 func releasem(mp *m) {
 	_g_ := getg()
 	mp.locks--
